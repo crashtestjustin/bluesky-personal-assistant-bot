63a54cd786750524ac03526ba4de2cdf7539418c
Justin Elliott <thecrashtestjustin@gmail.com>
2024-12-21 22:00:53 -0800

    added stat summarization and message sending to the stats

--- src/sendingMessage/sendMainActPostSummary.js
+++ src/sendingMessage/sendMainActPostSummary.js
@@ -1,56 +1,95 @@
+import { fetchPosts } from "../data/getDailyPostStats.js";
+
 //function to get main post data for the day and send out a message that summarizes engagement
-export async function sendAccountPostSummary(handle, session, accountPDS) {
-  //GET posts as an array
-  const delay = (ms) => new Promise((resolve) => setTimeout(resolve, ms));
+export async function sendAccountPostSummary(
+  handle,
+  session,
+  accountPDS,
+  convoId,
+  proxyHeader
+) {
+  //GET posts from past 24 hours as an array
 
-  const fetchPosts = async (url, actor) => {
-    let cursor = null;
-
-    // do {
-    try {
-      const params = new URLSearchParams({
-        actor: actor,
-        limit: "100",
-        ...(cursor && { cursor }),
-      });
-
-      const response = await fetch(`${accountPDS}/xrpc/${url}?${params}`, {
-        method: "GET",
-        headers: {
-          Authorization: `Bearer ${session.accessJwt}`,
-          "Content-Type": "application/json",
-        },
-      });
-
-      if (!response.ok) {
-        const errorData = await response.json();
-        throw new Error(
-          `Error fetching ${url}: ${response.status} - ${errorData?.error}`
-        );
-      }
-
-      const respData = await response.json();
-      //   console.log(respData.feed[0]);
-
-      const lastDay = new Date(Date.now() - 1000 * 60 * 60 * 24);
-
-      const recentPosts = respData.feed.filter(
-        (post) => post.post.author.createdAt >= lastDay
-      );
-      console.log(recentPosts);
-
-      await delay(500); // Add delay to avoid rate limits
-    } catch (error) {
-      console.error(`Failed to fetch handles: ${error.message}`);
-      // break; // Exit loop on failure
-    }
-    // } while (cursor);
-
-    return;
-  };
-
-  const jdePosts = await fetchPosts("app.bsky.feed.getAuthorFeed", handle);
+  const jdePosts = await fetchPosts(
+    "app.bsky.feed.getAuthorFeed",
+    handle,
+    accountPDS,
+    session
+  );
+  // console.log(jdePosts[0]);
 
   //total up the posts engagement - likes, reposts, replies, number of posts
+  const stats = {
+    totalLike: 0,
+    totalReposts: 0,
+    totalReplies: 0,
+    totalReplyOthers: 0,
+    totalRepostOthers: 0,
+  };
+
+  for (const post of jdePosts) {
+    stats.totalLike += post.post.likeCount;
+    stats.totalReplies += post.post.replyCount;
+    stats.totalReposts += post.post.repostCount;
+    post.post.record.embed && (stats.totalRepostOthers += 1);
+    post.post.record.reply && (stats.totalReplyOthers += 1);
+    // console.log(stats);
+  }
+
   //send a message that summarizes the information
+  const sendUpdateMessage = async () => {
+    const text = await messageText(stats);
+
+    const url = "chat.bsky.convo.sendMessage";
+
+    try {
+      const resp = await fetch(`${accountPDS}/xrpc/${url}`, {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+          Accept: "application/json",
+          Authorization: `Bearer ${session.accessJwt}`,
+          "Atproto-Proxy": proxyHeader,
+        },
+        body: JSON.stringify({
+          convoId: convoId,
+          message: {
+            text: text,
+          },
+        }),
+      });
+
+      const respsonse = await resp.json();
+      if (resp.ok) {
+        // console.log("message sent successfully", respsonse);
+        console.log("posts summary message sent successfully");
+      } else {
+        console.log("issue sending messsage", respsonse);
+      }
+    } catch (error) {
+      console.error("Error occurred while sending the message:", error);
+    }
+  };
+
+  const messageText = (stats) => {
+    return `
+    ---- 🙌Your personal post summary for today💪 ----\n\n${
+      stats.totalLike > 0
+        ? `• Total likes on posts: ${stats.totalLike}`
+        : "• No likes on posts today"
+    }\n${
+      stats.totalReplies > 0
+        ? `• Total replies on posts: ${stats.totalReplies} `
+        : "• No replies on posts today"
+    }\n${
+      stats.totalReposts > 0
+        ? `• Total reposts on posts: ${stats.totalReposts}`
+        : "• No reposts of your posts today"
+    }\n\nOn your end, you replied to ${
+      stats.totalReplyOthers
+    } posts and reposts/quote posted ${stats.totalRepostOthers} posts!
+    `;
+  };
+
+  const summaryMessage = sendUpdateMessage();
 }
